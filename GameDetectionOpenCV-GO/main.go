094eccaab1c16bb2383b0e74c00e4601d8117f59
Mircea Sorin Sebastian <mircea.sorin.sebastian@gmail.com>
2018-10-10 16:00:40 +0300

    Changed approach of contour filtering (only nodes that have exactly 9 direct kids)

--- GameDetectionOpenCV-GO/main.go
+++ GameDetectionOpenCV-GO/main.go
@@ -4,7 +4,6 @@ import (
 	"DetectPKG"
 	"fmt"
 	"gocv.io/x/gocv"
-	"strconv"
 )
 
 func computeFromFile(path string) (gocv.Mat, [3][3]int) {
@@ -17,45 +16,42 @@ func computeFromFile(path string) (gocv.Mat, [3][3]int) {
 
 func main() {
 	//// File mode
-	////img, board := computeFromFile("/Users/so/Desktop/XAnd0/GameDetectionOpenCV-GO/workingimg41.png")
-	//absPath, _ := filepath.Abs("./img.png")
-	//img, board := computeFromFile(absPath)
-	////img, board := computeFromFile("/Users/so/Desktop/XAnd0/GameDetectionOpenCV-GO/tests/testData/imgs/3.png")
-	//
-	//// Show the board
-	//fmt.Println(board)
-	//
-	//// Show the image
-	//finalImageWindow := gocv.NewWindow("FinalImage")
-	//finalImageWindow.IMShow(img)
-	//finalImageWindow.WaitKey(200000)
+	img, board := computeFromFile("/Users/so/Desktop/XAnd0/GameDetectionOpenCV-GO/tests/testData/imgs/2.png")
+
+	// Show the board
+	fmt.Println(board)
+
+	// Show the image
+	finalImageWindow := gocv.NewWindow("FinalImage")
+	finalImageWindow.IMShow(img)
+	finalImageWindow.WaitKey(200000)
 
 
 	//Webcam mode
-	webcam, _ := gocv.VideoCaptureDevice(0)
-	img := gocv.NewMat()
-	counter := 0
-	for {
-		//Read from webcam
-		webcam.Read(&img)
-		gocv.IMWrite("workingimgv2" + strconv.Itoa(counter) + ".png", img)
-		counter++
-
-		//Process the image
-		var dtct DetectPKG.IDetect
-		dtct = DetectPKG.DetectBuilder(img)
-		img, board := dtct.Detect()
-
-		// Show the board
-		fmt.Println(board)
-
-	//	// Show the image
-		finalImageWindow := gocv.NewWindow("FinalImage")
-		finalImageWindow.IMShow(img)
-		finalImageWindow.WaitKey(2000)
-	}
-
-	// https://www.geeksforgeeks.org/longest-path-between-any-pair-of-vertices/
+	//webcam, _ := gocv.VideoCaptureDevice(0)
+	//img := gocv.NewMat()
+	//counter := 0
+	//for {
+	//	//Read from webcam
+	//	webcam.Read(&img)
+	//	gocv.IMWrite("workingimgv2" + strconv.Itoa(counter) + ".png", img)
+	//	counter++
+	//
+	//	//Process the image
+	//	var dtct DetectPKG.IDetect
+	//	dtct = DetectPKG.DetectBuilder(img)
+	//	img, board := dtct.Detect()
+	//
+	//	// Show the board
+	//	fmt.Println(board)
+	//
+	////	// Show the image
+	//	finalImageWindow := gocv.NewWindow("FinalImage")
+	//	finalImageWindow.IMShow(img)
+	//	finalImageWindow.WaitKey(2000)
+	//}
+	//
+	//// https://www.geeksforgeeks.org/longest-path-between-any-pair-of-vertices/
 }
 
 //img = gocv.IMRead("/Users/so/Desktop/HexaXand0/GameDetectionOpenCV-GO/img.jpeg", gocv.IMReadColor)  // To read from file
